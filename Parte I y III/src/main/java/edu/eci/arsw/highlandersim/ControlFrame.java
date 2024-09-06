a9f5a2c25d5e4029e103a07f56b603ab8923819a
johanestrada01 <johan.estrada-p@mail.escuelaing.edu.co>
2024-09-06 11:04:36 -0500

    Eliminacion de hilos

--- Parte I y III/src/main/java/edu/eci/arsw/highlandersim/ControlFrame.java
+++ Parte I y III/src/main/java/edu/eci/arsw/highlandersim/ControlFrame.java
@@ -1,28 +1,25 @@
 package edu.eci.arsw.highlandersim;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JOptionPane;
-import javax.swing.JToolBar;
 import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollBar;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-
-import javax.swing.JLabel;
 import javax.swing.JTextField;
-import java.awt.Color;
-import javax.swing.JScrollBar;
+import javax.swing.JToolBar;
+import javax.swing.border.EmptyBorder;
 
 public class ControlFrame extends JFrame {
 
@@ -113,10 +110,12 @@ public class ControlFrame extends JFrame {
         btnResume.addActionListener(new ActionListener() {
             public void actionPerformed (ActionEvent e){
             synchronized (lock) {
-                for (Immortal i : immortals) {
-                    i.resumeThread();
-                }
-                lock.notifyAll();
+                synchronized (immortals) {
+                    for (Immortal i : immortals) {
+                        i.resumeThread();
+                    }
+                    lock.notifyAll();
+                } 
             }
         }
         });
